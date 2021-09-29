64eda22581133d725799fe258027f37001618a6e
Kun Qin <91503737+kqin050@users.noreply.github.com>
2021-09-29 16:30:29 -0400

    pull commit from github

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -12,6 +12,7 @@ public class GitTest {
         System.out.println("Hello Git4");
         System.out.println("Hello Master");
         System.out.println("Hello hot-fix");
-        System.out.println("git hub push test");
+        System.out.println("git hub push test"); 
+        System.out.println("git hub pull test from web");
     }
 }
