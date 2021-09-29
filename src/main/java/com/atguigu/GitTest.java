90a2254e76f1f0805a643bf45ed71691aaac5d03
Kun <messiakun@foxmail.com>
2021-09-29 16:03:55 -0400

    first commit

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author kqin050_uottawa
+ * @create 2021-09-29 15:54
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
